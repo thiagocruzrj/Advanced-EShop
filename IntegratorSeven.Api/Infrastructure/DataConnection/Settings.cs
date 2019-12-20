1ee86398d4a03af3af94a286f3bc2494961b02c4
Thiago Cruz <thagocruz@gmail.com>
2019-12-20 11:41:20 -0300

    Update Settings.cs

--- IntegratorSeven.Api/Infrastructure/DataConnection/Settings.cs
+++ IntegratorSeven.Api/Infrastructure/DataConnection/Settings.cs
@@ -2,6 +2,6 @@ namespace IntegratorSeven.Api.Infrastructure.DataConnection
 {
     public static class Settings
     {
-        public static string ConnectionString = @"jdbc:oracle:thin:bi_read/bi#brmalls#26@192.168.100.170:1521/BRMBIPRD";
+        public static string ConnectionString = @"";
     }
 }
