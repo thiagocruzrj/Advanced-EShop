5233145762ddb809747619e69210815c21568170
Thiago Cruz <thagocruz@gmail.com>
2020-05-02 20:52:21 -0300

    Remove uncessecery usings

--- src/Services/AES.Identity.API/Controllers/AuthController.cs
+++ src/Services/AES.Identity.API/Controllers/AuthController.cs
@@ -9,9 +9,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
-using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 using System.Linq;
-using System.Collections.Generic;
 
 namespace AES.Identity.API.Controllers
 {
