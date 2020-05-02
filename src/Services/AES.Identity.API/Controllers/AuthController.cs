f82a4d9b2abafcd5a22d35e73b34369bd98fffa6
Thiago Cruz <thagocruz@gmail.com>
2020-05-02 14:49:34 -0300

    Add first validation and add new user instance

--- src/Services/AES.Identity.API/Controllers/AuthController.cs
+++ src/Services/AES.Identity.API/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using AES.Identity.API.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -6,7 +7,21 @@ namespace AES.Identity.API.Controllers
 {
     public class AuthController : Controller
     {
-        public async Task<ActionResult> Register(UserRegister userRegister) { }
-        public async Task<ActionResult> Login(UserLogin userLogin) { }
+        public async Task<ActionResult> Register(UserRegister userRegister) 
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = userRegister.Email,
+                Email = userRegister.Email,
+                EmailConfirmed = true
+            };
+        }
+
+        public async Task<ActionResult> Login(UserLogin userLogin)
+        {
+
+        }
     }
 }
