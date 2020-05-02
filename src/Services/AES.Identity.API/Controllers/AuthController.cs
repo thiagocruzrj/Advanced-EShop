70e816a65a5488b7721fdd4513db279326f2c043
Thiago Cruz <thagocruz@gmail.com>
2020-05-02 14:46:24 -0300

    Add base methods on auth controller

---
+++ src/Services/AES.Identity.API/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using AES.Identity.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AES.Identity.API.Controllers
+{
+    public class AuthController : Controller
+    {
+        public async Task<ActionResult> Register(UserRegister userRegister) { }
+        public async Task<ActionResult> Login(UserLogin userLogin) { }
+    }
+}
