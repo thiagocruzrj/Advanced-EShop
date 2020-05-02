5046faa01c6c5cad45da24ff2c82d0f738c5863d
Thiago Cruz <thagocruz@gmail.com>
2020-05-02 14:43:46 -0300

    Add data annotations to validate request

--- src/Services/AES.Identity.API/Models/UserViewModels.cs
+++ src/Services/AES.Identity.API/Models/UserViewModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +8,26 @@ namespace AES.Identity.API.Models
 {
     public class UserRegister
     {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "The field {0} is in an invalid format")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(25, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 6)]
         public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "The password don't match")]
         public string PasswordConfirmation { get; set; }
     }
 
     public class UserLogin
     {
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [EmailAddress(ErrorMessage = "The field {0} is in an invalid format.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(25, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 6)]
         public string Password { get; set; }
     }
 }
