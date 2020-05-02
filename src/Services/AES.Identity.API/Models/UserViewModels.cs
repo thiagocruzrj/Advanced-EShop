7de32f121cbc4a7924d6f82b0f5c19ed7eb9072f
Thiago Cruz <thagocruz@gmail.com>
2020-05-02 14:37:22 -0300

    Add register user model

---
+++ src/Services/AES.Identity.API/Models/UserViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AES.Identity.API.Models
+{
+    public class UserRegister
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirmation { get; set; }
+    }
+
+    public class UserLogin
+    {
+    }
+}
