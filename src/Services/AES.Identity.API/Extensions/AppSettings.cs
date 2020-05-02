6b45cc0b652cf9f24074d834f626f6daf7d33ecd
Thiago Cruz <thagocruz@gmail.com>
2020-05-02 17:13:17 -0300

    Add base token setting

---
+++ src/Services/AES.Identity.API/Extensions/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AES.Identity.API.Extensions
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+        public int ExpiryHours { get; set; }
+        public string Issuer { get; set; }
+        public string ValidOn { get; set; }
+    }
+}
