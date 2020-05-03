339ffae581d506c72a3f0a75bc4446e30576d72b
Thiago Cruz <thagocruz@gmail.com>
2020-05-03 14:38:26 -0300

    Add jwt config

--- src/Services/AES.Identity.API/Configuration/IdentityConfig.cs
+++ src/Services/AES.Identity.API/Configuration/IdentityConfig.cs
@@ -1,11 +1,15 @@
 ﻿using AES.Identity.API.Data;
+using AES.Identity.API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Text;
 
 namespace AES.Identity.API.Configuration
 {
@@ -29,6 +33,33 @@ namespace AES.Identity.API.Configuration
                     Contact = new OpenApiContact() { Name = "Thiago Cruz", Email = "thagocruz@gmail.com" },
                     License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensourse.org/licenses/MIT") }
                 });
+            });
+
+            // JWT
+
+            var appSettingSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingSection);
+
+            var appSettings = appSettingSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(baererOptions =>
+            {
+                baererOptions.RequireHttpsMetadata = true;
+                baererOptions.SaveToken = true;
+                baererOptions.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidOn,
+                    ValidIssuer = appSettings.Issuer
+                };
             });
         }
 
