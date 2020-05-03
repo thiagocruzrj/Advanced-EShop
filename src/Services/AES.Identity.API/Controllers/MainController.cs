efd2bb08c065dc578f27f106b88a8f3fb66cb885
Thiago Cruz <thagocruz@gmail.com>
2020-05-03 13:27:02 -0300

    Add errors details

--- src/Services/AES.Identity.API/Controllers/MainController.cs
+++ src/Services/AES.Identity.API/Controllers/MainController.cs
@@ -1,9 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AES.Identity.API.Controllers
 {
     [ApiController]
-    public class MainController : Controller
+    public abstract class MainController : Controller
     {
+        public ICollection<string> Errors = new List<string>();
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (ValidOperation())
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+            {
+                { "Mesages", Errors.ToArray() }
+            }));
+        }
+
+        protected bool ValidOperation()
+        {
+            return !Errors.Any();
+        }
     }
 }
