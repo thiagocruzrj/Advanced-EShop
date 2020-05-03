ecd1559b2e717d5a662707a0ade3b0c78b1ff83e
Thiago Cruz <thagocruz@gmail.com>
2020-05-03 14:40:45 -0300

    Refactor startup identity part

--- src/Services/AES.Identity.API/Startup.cs
+++ src/Services/AES.Identity.API/Startup.cs
@@ -39,52 +39,8 @@ namespace AES.Identity.API
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-
-            services.AddSwaggerGen(c => 
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "Advanced Enterprise Shop Api",
-                    Description = "Api created to increase base microservices communication skills",
-                    Contact = new OpenApiContact() { Name = "Thiago Cruz", Email = "thagocruz@gmail.com" },
-                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensourse.org/licenses/MIT") }
-                });
-            });
-
+            services.AddIdentityConfiguration(Configuration);
             services.AddApiConfiguration();
-
-            // JWT
-
-            var appSettingSection = Configuration.GetSection("AppSettings");
-            services.Configure<AppSettings>(appSettingSection);
-
-            var appSettings = appSettingSection.Get<AppSettings>();
-            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-
-            services.AddAuthentication(o =>
-            {
-                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(baererOptions => 
-            {
-                baererOptions.RequireHttpsMetadata = true;
-                baererOptions.SaveToken = true;
-                baererOptions.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidAudience = appSettings.ValidOn,
-                    ValidIssuer = appSettings.Issuer
-                };
-            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -95,6 +51,7 @@ namespace AES.Identity.API
                 o.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
             });
 
+            app.UseIdentityConfiguration();
             app.UseApiConfiguration(env);
         }
     }
