4c2c1f25df3f4e594412a3fcebe8b76fe85c1b7e
Thiago Cruz <thagocruz@gmail.com>
2020-05-03 14:35:31 -0300

    Add config methods

--- src/Services/AES.Identity.API/Configuration/IdentityConfig.cs
+++ src/Services/AES.Identity.API/Configuration/IdentityConfig.cs
@@ -1,6 +1,17 @@
-﻿namespace AES.Identity.API.Configuration
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AES.Identity.API.Configuration
 {
-    public class IdentityConfig
+    public static class IdentityConfig
     {
+        public static void AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+        }
+
+        public static void UseIdentityConfiguration(this IApplicationBuilder app)
+        {
+        }
     }
 }
