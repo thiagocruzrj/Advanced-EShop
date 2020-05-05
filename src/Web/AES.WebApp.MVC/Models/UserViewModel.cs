15816258d2f1d4a239ba026a8c77ed142305f418
Thiago Cruz <thagocruz@gmail.com>
2020-05-05 13:00:29 -0300

    Add annotations on properties

--- src/Web/AES.WebApp.MVC/Models/UserViewModel.cs
+++ src/Web/AES.WebApp.MVC/Models/UserViewModel.cs
@@ -1,15 +1,30 @@
-﻿namespace AES.WebApp.MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AES.WebApp.MVC.Models
 {
     public class UserRegister
     {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "The field {0} has an invalid format")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(20, ErrorMessage = "The field {0} must be between {2} and {1} characteres", MinimumLength = 6)]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [Compare("Password", ErrorMessage = "Passwords don't match")]
         public string PasswordConfirmation { get; set; }
     }
 
     public class UserLogin
     {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "The field {0} has an invalid format")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(20, ErrorMessage = "The field {0} must be between {2} and {1} characteres", MinimumLength = 6)]
         public string Password { get; set; }
     }
 }
