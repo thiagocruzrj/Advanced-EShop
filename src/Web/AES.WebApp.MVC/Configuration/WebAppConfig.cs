485e83a5a82e1099847581715f624ce176614b5b
Thiago Cruz <thagocruz@gmail.com>
2020-05-05 12:36:55 -0300

    Add webApp extensions

---
+++ src/Web/AES.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AES.WebApp.MVC.Configuration
+{
+    public static class WebAppConfig
+    {
+        public static void AddWebAppConfig(this IServiceCollection services)
+        {
+        }
+
+        public static void UseWebAppConfig(this IApplicationBuilder app)
+        {
+        }
+    }
+}
