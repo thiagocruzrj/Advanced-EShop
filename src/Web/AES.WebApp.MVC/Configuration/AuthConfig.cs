5bb5f2a1b4b2a105f1bee9f293a12f45d37a0c9a
Thiago Cruz <thagocruz@gmail.com>
2020-05-05 12:27:06 -0300

    Add auth extension methods

---
+++ src/Web/AES.WebApp.MVC/Configuration/AuthConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AES.WebApp.MVC.Configuration
+{
+    public static class AuthConfig
+    {
+        public static void AddAuthConfig(this IServiceCollection services)
+        {
+
+        }
+
+        public static void UseAuthConfig(this IApplicationBuilder app)
+        {
+
+        }
+    }
+}
