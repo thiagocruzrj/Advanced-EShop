60ec866b0f3fcd6800cf9353dbccb0106900da11
Thiago Cruz <thagocruz@gmail.com>
2020-05-05 12:38:05 -0300

    Finiishing base extensions from webApp

--- src/Web/AES.WebApp.MVC/Configuration/WebAppConfig.cs
+++ src/Web/AES.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace AES.WebApp.MVC.Configuration
 {
@@ -7,10 +9,31 @@ namespace AES.WebApp.MVC.Configuration
     {
         public static void AddWebAppConfig(this IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
-        public static void UseWebAppConfig(this IApplicationBuilder app)
+        public static void UseWebAppConfig(this IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
