d1a49108c3efdbbaac9946bdc417a737ac37d1bf
Thiago Cruz <thagocruz@gmail.com>
2020-05-05 13:04:52 -0300

    Add necessary methods on controller

--- src/Web/AES.WebApp.MVC/Controllers/AuthController.cs
+++ src/Web/AES.WebApp.MVC/Controllers/AuthController.cs
@@ -1,8 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AES.WebApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace AES.WebApp.MVC.Controllers
 {
     public class AuthController : Controller
     {
+        [HttpPost]
+        [Route("new-account")]
+        public async Task<ActionResult> Register(UserRegister userRegister) { }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login() { return View(); }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UserLogin userLogin) { }
+
+        [HttpGet]
+        [Route("exit")]
+        public async Task<IActionResult> Logout() { }
     }
 }
