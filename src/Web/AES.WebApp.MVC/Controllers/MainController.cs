87707589edcce1c4703a5905a85af365dde80933
Thiago Cruz <thagocruz@gmail.com>
2020-05-09 18:24:01 -0300

    Verify errors on response

---
+++ src/Web/AES.WebApp.MVC/Controllers/MainController.cs
@@ -0,0 +1,18 @@
+﻿using AES.WebApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace AES.WebApp.MVC.Controllers
+{
+    public class MainController : Controller
+    {
+        protected bool ResponseHasErrors(ResponseResult response)
+        {
+            if (response != null && response.Errors.Messages.Any())
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
