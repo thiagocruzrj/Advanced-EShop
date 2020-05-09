19e63a5bbdce1d85bb63e58320bff7d832380f11
Thiago Cruz <thagocruz@gmail.com>
2020-05-09 13:51:34 -0300

    Create auth service class

---
+++ src/Web/AES.WebApp.MVC/Service/AuthService.cs
@@ -0,0 +1,18 @@
+﻿using AES.WebApp.MVC.Models;
+using System.Threading.Tasks;
+
+namespace AES.WebApp.MVC.Service
+{
+    public class AuthService : IAuthService
+    {
+        public Task<string> Login(UserLogin userLogin)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<string> Register(UserRegister userRegister)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
