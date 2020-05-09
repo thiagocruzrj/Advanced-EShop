07f5ac1ff0716c3c5b50b905bdd84200eb63cada
Thiago Cruz <thagocruz@gmail.com>
2020-05-09 14:07:17 -0300

    Register auth service

---
+++ src/Web/AES.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,13 @@
+﻿using AES.WebApp.MVC.Service;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AES.WebApp.MVC.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddHttpClient<IAuthService, AuthService>();
+        }
+    }
+}
