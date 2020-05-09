e2bc9f64db0c0c80d7bee565571b8c870d0a593f
Thiago Cruz <thagocruz@gmail.com>
2020-05-09 13:49:16 -0300

    Create auth service

---
+++ src/Web/AES.WebApp.MVC/Service/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using AES.WebApp.MVC.Models;
+using System.Threading.Tasks;
+
+namespace AES.WebApp.MVC.Service
+{
+    public interface IAuthService
+    {
+        Task<string> Login(UserLogin userLogin);
+        Task<string> Register(UserRegister userRegister);
+    }
+}
