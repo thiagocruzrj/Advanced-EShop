c281c3777c2db4b4329e12537333cdc66d22125f
Thiago Cruz <thagocruz@gmail.com>
2020-05-09 13:55:27 -0300

    Create login content on request

--- src/Web/AES.WebApp.MVC/Service/AuthService.cs
+++ src/Web/AES.WebApp.MVC/Service/AuthService.cs
@@ -1,13 +1,27 @@
 ﻿using AES.WebApp.MVC.Models;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace AES.WebApp.MVC.Service
 {
     public class AuthService : IAuthService
     {
+        private readonly HttpClient _httpClient;
+
+        public AuthService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public Task<string> Login(UserLogin userLogin)
         {
-            throw new System.NotImplementedException();
+            var loginContent = new StringContent(
+                JsonSerializer.Serialize(userLogin),
+                Encoding.UTF8,
+                "application/json");
+            var response = _httpClient.PostAsync("", loginContent);
         }
 
         public Task<string> Register(UserRegister userRegister)
