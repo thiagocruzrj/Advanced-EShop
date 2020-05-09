9ed352bf13ddb1118749b5c4a6aff81a7e524d15
Thiago Cruz <thagocruz@gmail.com>
2020-05-09 17:35:16 -0300

    Add new services

--- src/Web/AES.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/Web/AES.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,6 @@
-﻿using AES.WebApp.MVC.Service;
+﻿using AES.WebApp.MVC.Extensions;
+using AES.WebApp.MVC.Service;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AES.WebApp.MVC.Configuration
@@ -8,6 +10,9 @@ namespace AES.WebApp.MVC.Configuration
         public static void RegisterServices(this IServiceCollection services)
         {
             services.AddHttpClient<IAuthService, AuthService>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IUser, AspNetUser>();
         }
     }
 }
