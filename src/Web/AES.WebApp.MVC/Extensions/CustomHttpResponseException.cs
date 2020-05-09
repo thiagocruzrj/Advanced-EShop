60999c9055a54ac93d584939096d60e7f8e0ef0a
Thiago Cruz <thagocruz@gmail.com>
2020-05-09 18:16:18 -0300

    Add custon base exception

---
+++ src/Web/AES.WebApp.MVC/Extensions/CustomHttpResponseException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+
+namespace AES.WebApp.MVC.Extensions
+{
+    public class CustomHttpResponseException : Exception
+    {
+        public HttpStatusCode StatusCode;
+
+        public CustomHttpResponseException() { }
+        public CustomHttpResponseException(string message, Exception innerException) : base (message, innerException) { }
+        public CustomHttpResponseException(HttpStatusCode statusCode) { StatusCode = statusCode; }
+    }
+}
