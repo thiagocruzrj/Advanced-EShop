15ca65c48c775f36730c01d8eca1a9a87833a0a8
Thiago Cruz <thagocruz@gmail.com>
2020-05-13 20:05:39 -0300

    Add product dto

---
+++ src/Services/AES.Catalog.API/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AES.Catalog.API.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+        public decimal Price { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public string Image { get; set; }
+        public int Amount { get; set; }
+    }
+}
