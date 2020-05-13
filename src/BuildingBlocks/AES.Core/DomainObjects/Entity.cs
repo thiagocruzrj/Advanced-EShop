77683729e9af047cdc628c26f9c015fb397559fd
Thiago Cruz <thagocruz@gmail.com>
2020-05-13 20:13:15 -0300

    Override equals method on Entity core class

--- src/BuildingBlocks/AES.Core/DomainObjects/Entity.cs
+++ src/BuildingBlocks/AES.Core/DomainObjects/Entity.cs
@@ -5,5 +5,15 @@ namespace AES.Core.DomainObjects
     public abstract class Entity
     {
         public Guid Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
     }
 }
