4ac3977b784f851cc313aae2ac90bc022a64b85a
Thiago Cruz <thagocruz@gmail.com>
2020-05-13 20:32:30 -0300

    Add Entity on products

--- src/Services/AES.Catalog.API/Models/Product.cs
+++ src/Services/AES.Catalog.API/Models/Product.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using AES.Core.DomainObjects;
+using System;
 
 namespace AES.Catalog.API.Models
 {
-    public class Product
+    public class Product : Entity
     {
         public string Name { get; set; }
         public string Description { get; set; }
