19a2f0c7a829e58150791eb0d330081942d8e0d5
Thiago Cruz <thagocruz@gmail.com>
2020-05-13 20:27:52 -0300

    Implement new operation behavior

--- src/BuildingBlocks/AES.Core/DomainObjects/Entity.cs
+++ src/BuildingBlocks/AES.Core/DomainObjects/Entity.cs
@@ -16,6 +16,22 @@ namespace AES.Core.DomainObjects
             return Id.Equals(compareTo.Id);
         }
 
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
         public override int GetHashCode()
         {
             return (GetType().GetHashCode() * 907) + Id.GetHashCode();
