12588efd78ff73b7c1ed19606d7a994a3058b688
Thiago Cruz <thagocruz@gmail.com>
2020-05-16 17:35:46 -0300

    change configure startup

--- src/Services/AES.Catalog.API/Startup.cs
+++ src/Services/AES.Catalog.API/Startup.cs
@@ -33,26 +33,14 @@ namespace AES.Catalog.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApiConfiguration(Configuration);
+
+            services.RegisterServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseApiConfiguration(env);
         }
     }
 }
