97034384b7f6aaaaf013dd0a67e65b5962ed9483
Thiago Cruz <thagocruz@gmail.com>
2020-05-16 15:48:57 -0300

    Add methods to implement on repo

---
+++ src/Services/AES.Catalog.API/Data/Repository/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using AES.Catalog.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AES.Catalog.API.Data.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<IEnumerable<Product>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
