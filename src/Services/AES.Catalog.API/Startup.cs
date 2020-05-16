a8a83c2948c9b0ccbc6c1745089c98a74bce4b6f
Thiago Cruz <thagocruz@gmail.com>
2020-05-16 17:51:32 -0300

    Add seed products

--- src/Services/AES.Catalog.API/Startup.cs
+++ src/Services/AES.Catalog.API/Startup.cs
@@ -1,10 +1,6 @@
 using AES.Catalog.API.Configuration;
-using AES.Catalog.API.Data;
-using AES.Catalog.API.Data.Repository;
-using AES.Catalog.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
