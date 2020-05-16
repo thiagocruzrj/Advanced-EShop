be55aff8cbfa28be04e4dbe20320b6ecf99ffe63
Thiago Cruz <thagocruz@gmail.com>
2020-05-16 17:16:48 -0300

    Add basic catalog controller

---
+++ src/Services/AES.Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,16 @@
+﻿using AES.Catalog.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AES.Catalog.API.Controllers
+{
+    [ApiController]
+    public class CatalogController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public CatalogController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+    }
+}
