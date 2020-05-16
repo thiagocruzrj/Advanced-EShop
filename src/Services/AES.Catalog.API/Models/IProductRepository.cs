bfb13bdd0357e614aad4f908fc51b4ba882460ea
Thiago Cruz <thagocruz@gmail.com>
2020-05-16 15:46:28 -0300

    Add some methods on interface

--- src/Services/AES.Catalog.API/Models/IProductRepository.cs
+++ src/Services/AES.Catalog.API/Models/IProductRepository.cs
@@ -1,8 +1,16 @@
 ﻿using AES.Core.DomainObjects;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AES.Catalog.API.Models
 {
     public interface IProductRepository : IRepository<Product>
     {
+        Task<IEnumerable<Product>> GetAll();
+        Task<Product> GetById(Guid id);
+        void Add(Product product);
+        void Update(Product product);
     }
 }
