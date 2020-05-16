c3a4246750402bd94831ac9334134ab2eaa9abd9
Thiago Cruz <thagocruz@gmail.com>
2020-05-16 17:24:05 -0300

    Add api configuration static class

---
+++ src/Services/AES.Catalog.API/Configuration/ApiConfig.cs
@@ -0,0 +1,18 @@
+﻿using AES.Catalog.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AES.Catalog.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CatalogContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+        }
+    }
+}
---
+++ src/Services/AES.Catalog.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AES.Catalog.API.Configuration
+{
+    public class DependencyInjectionConfig
+    {
+    }
+}
