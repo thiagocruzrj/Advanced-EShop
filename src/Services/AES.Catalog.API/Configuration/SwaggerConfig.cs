cd29015072c7c5770210ee9575dbf3cb1a8b7285
Thiago Cruz <thagocruz@gmail.com>
2020-05-16 17:41:40 -0300

    Swagger configuuration

--- src/Services/AES.Catalog.API/Configuration/SwaggerConfig.cs
+++ src/Services/AES.Catalog.API/Configuration/SwaggerConfig.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AES.Catalog.API.Configuration
 {
-    public class SwaggerConfig
+    public static class SwaggerConfig
     {
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Advanced Enterprise Catalog API",
+                    Description = "Api create to manage a shop catalog",
+                    Contact = new OpenApiContact() { Name = "Thiago Cruz", Email = "thagocruz@gmail.com"},
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT")}
+                });
+            });
+        }
     }
 }
