d94d449b88ae3249bc53c583826568f3e873c0e1
Thiago Cruz <thagocruz@gmail.com>
2020-05-16 17:29:26 -0300

    Add use api configuration extension method

--- src/Services/AES.Catalog.API/Configuration/ApiConfig.cs
+++ src/Services/AES.Catalog.API/Configuration/ApiConfig.cs
@@ -1,7 +1,10 @@
 ﻿using AES.Catalog.API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace AES.Catalog.API.Configuration
 {
@@ -13,6 +16,34 @@ namespace AES.Catalog.API.Configuration
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
+
+            services.AddCors(o => 
+            {
+                o.AddPolicy("Total",
+                    builder => builder
+                               .AllowAnyOrigin()
+                               .AllowAnyMethod()
+                               .AllowAnyHeader());
+            });
+        }
+
+        public static void UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors("Total");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
