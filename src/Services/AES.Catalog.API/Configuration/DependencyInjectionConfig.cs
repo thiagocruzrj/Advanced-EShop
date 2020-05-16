4473ab7af99630e8a2d799af343f7e9510286738
Thiago Cruz <thagocruz@gmail.com>
2020-05-16 17:31:04 -0300

    Add di register services extension method

--- src/Services/AES.Catalog.API/Configuration/DependencyInjectionConfig.cs
+++ src/Services/AES.Catalog.API/Configuration/DependencyInjectionConfig.cs
@@ -1,11 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AES.Catalog.API.Data;
+using AES.Catalog.API.Data.Repository;
+using AES.Catalog.API.Models;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AES.Catalog.API.Configuration
 {
-    public class DependencyInjectionConfig
+    public static class DependencyInjectionConfig
     {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<CatalogContext>();
+        }
     }
 }
