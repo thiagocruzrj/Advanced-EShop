931f8a1160f89177b050cfda847fadae227c4cbd
Thiago Cruz <thagocruz@gmail.com>
2020-05-16 14:21:42 -0300

    Add CatalogDbContext

---
+++ src/Services/AES.Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,12 @@
+﻿using AES.Catalog.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AES.Catalog.API.Data
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options) { }
+
+        public DbSet<Product> Products {get; set;}
+    }
+}
