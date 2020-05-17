2cc8a91f646562b0016c41619b795a26dd9d76cb
Thiago Cruz <thagocruz@gmail.com>
2020-05-16 23:26:12 -0300

    Add catalog interface on mvc layer

---
+++ src/Web/AES.WebApp.MVC/Service/ICatalogService.cs
@@ -0,0 +1,13 @@
+﻿using AES.WebApp.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AES.WebApp.MVC.Service
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<ProductViewModel>> GetAll();
+        Task<ProductViewModel> GetById(Guid id);
+    }
+}
