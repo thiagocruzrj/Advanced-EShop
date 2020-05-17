ae76b12e010cc91c4394da837700130e76c9d238
Thiago Cruz <thagocruz@gmail.com>
2020-05-16 23:08:08 -0300

    Add catalog controller base on mvc layer

---
+++ src/Services/AES.Identity.API/Controllers/CatalogController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace AES.Identity.API.Controllers
+{
+    public class CatalogController : MainController
+    {
+        [HttpGet]
+        [Route("")]
+        [Route("showcase")]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("product-detail/{id}")]
+        public async Task<IActionResult> ProductDetail(Guid id)
+        {
+            return View();
+        }
+    }
+}
