1d850dea14f6dbf43f42e3f4616c3237f36f256f
Thiago Cruz <thagocruz@gmail.com>
2020-05-17 18:53:49 -0300

    Add new delegation handler to override send async

---
+++ src/Web/AES.WebApp.MVC/Service/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,38 @@
+﻿using AES.WebApp.MVC.Extensions;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AES.WebApp.MVC.Service.Handlers
+{
+    public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+    {
+        private readonly IUser _user;
+
+        public HttpClientAuthorizationDelegatingHandler(IUser user)
+        {
+            _user = user;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authorizationHeader = _user.GetHttpContext().Request.Headers["Authorization"];
+
+            if (!string.IsNullOrEmpty(authorizationHeader))
+            {
+                request.Headers.Add("Authorization", new List<string>() { authorizationHeader });
+            }
+
+            var token = _user.GetUserToken();
+
+            if (token != null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
