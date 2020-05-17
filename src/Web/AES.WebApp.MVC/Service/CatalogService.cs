1a53929eb31e0cc8745c475f0ad814fd8316be8e
Thiago Cruz <thagocruz@gmail.com>
2020-05-16 23:30:58 -0300

    Get by id implementation

--- src/Web/AES.WebApp.MVC/Service/CatalogService.cs
+++ src/Web/AES.WebApp.MVC/Service/CatalogService.cs
@@ -1,12 +1,24 @@
-﻿using AES.WebApp.MVC.Models;
+﻿using AES.WebApp.MVC.Extensions;
+using AES.WebApp.MVC.Models;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace AES.WebApp.MVC.Service
 {
-    public class CatalogService : ICatalogService
+    public class CatalogService : Service, ICatalogService
     {
+        private readonly HttpClient _httpClient;
+
+        public CatalogService(HttpClient httpClient,
+                           IOptions<AppSettings> settings)
+        {
+            httpClient.BaseAddress = new Uri(settings.Value.AuthUrl);
+            _httpClient = httpClient;
+        }
+
         public async Task<IEnumerable<ProductViewModel>> GetAll()
         {
             throw new NotImplementedException();
@@ -14,7 +26,9 @@ namespace AES.WebApp.MVC.Service
 
         public async Task<ProductViewModel> GetById(Guid id)
         {
-            throw new NotImplementedException();
+            var response = await _httpClient.GetAsync("/catalog/products");
+            HandlingErrorsReponse(response);
+            return await DeserializeObjectResponse<ProductViewModel>(response);
         }
     }
 }
