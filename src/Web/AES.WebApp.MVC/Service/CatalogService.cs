346d2a6ba151532618ff2ef922406309553613c3
Thiago Cruz <thagocruz@gmail.com>
2020-05-16 23:26:21 -0300

    Add catalog implementation

---
+++ src/Web/AES.WebApp.MVC/Service/CatalogService.cs
@@ -0,0 +1,20 @@
+﻿using AES.WebApp.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AES.WebApp.MVC.Service
+{
+    public class CatalogService : ICatalogService
+    {
+        public async Task<IEnumerable<ProductViewModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ProductViewModel> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
