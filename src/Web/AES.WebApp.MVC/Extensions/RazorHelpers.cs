d7413064ab96e8ed2f3b7d98a9be3a588c1f6620
Thiago Cruz <thagocruz@gmail.com>
2020-05-22 00:09:39 -0300

    Add method to hashing email

--- src/Web/AES.WebApp.MVC/Extensions/RazorHelpers.cs
+++ src/Web/AES.WebApp.MVC/Extensions/RazorHelpers.cs
@@ -1,10 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 
 namespace AES.WebApp.MVC.Extensions
 {
     public static class RazorHelpers
     {
+        public static string HashEmailForGravatar(this RazorPage page, string email)
+        {
+            var md5Hasher = MD5.Create();
+            var data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(email));
+            var sBuilder = new StringBuilder();
+            foreach (var t in data)
+            {
+                sBuilder.Append(t.ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
         public static string StockMessage(this RazorPage page, int amount)
         {
             return amount > 0 ? $"Only {amount} on stock!" : "Sold out product!";
