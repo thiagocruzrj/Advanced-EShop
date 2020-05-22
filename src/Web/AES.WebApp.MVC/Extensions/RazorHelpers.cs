b71117fa6bd6de6c46f5ae1e440d8f0d7e67016b
Thiago Cruz <thagocruz@gmail.com>
2020-05-21 23:54:07 -0300

    Add razor extension

---
+++ src/Web/AES.WebApp.MVC/Extensions/RazorHelpers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace AES.WebApp.MVC.Extensions
+{
+    public static class RazorHelpers
+    {
+        public static string StockMessage(this RazorPage page, int amount)
+        {
+            return amount > 0 ? $"Only {amount} on stock!" : "Sold out product";
+        }
+    }
+}
