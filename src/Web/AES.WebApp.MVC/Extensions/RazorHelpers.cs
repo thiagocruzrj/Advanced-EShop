a8724aac6748abb880fbb6a6f3473af193a7ab62
Thiago Cruz <thagocruz@gmail.com>
2020-05-22 00:06:26 -0300

    Add new coin format

--- src/Web/AES.WebApp.MVC/Extensions/RazorHelpers.cs
+++ src/Web/AES.WebApp.MVC/Extensions/RazorHelpers.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System.Threading;
 
 namespace AES.WebApp.MVC.Extensions
 {
@@ -6,7 +7,12 @@ namespace AES.WebApp.MVC.Extensions
     {
         public static string StockMessage(this RazorPage page, int amount)
         {
-            return amount > 0 ? $"Only {amount} on stock!" : "Sold out product";
+            return amount > 0 ? $"Only {amount} on stock!" : "Sold out product!";
+        }
+
+        public static string CoinFormat(this RazorPage page, decimal price)
+        {
+            return price > 0 ? string.Format(Thread.CurrentThread.CurrentCulture, "{0:C}", price) : "Free";
         }
     }
 }
