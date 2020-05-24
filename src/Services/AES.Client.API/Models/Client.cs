6a82ec235a55c23f7e9f9b239ef910c99abb3426
Thiago Cruz <thagocruz@gmail.com>
2020-05-23 23:36:11 -0300

    Add Client entity

---
+++ src/Services/AES.Client.API/Models/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AES.Client.API.Models
+{
+    public class Client
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public bool Excluded { get; private set; }
+        public Address Address { get; private set; }
+    }
+
+    public class Address
+    {
+    }
+}
