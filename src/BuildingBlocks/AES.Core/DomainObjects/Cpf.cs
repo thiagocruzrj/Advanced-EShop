f3f7e161d90f0ca4fb420b47bbf1ef995abfbf7f
Thiago Cruz <thagocruz@gmail.com>
2020-05-24 15:55:32 -0300

    Create value object cpf

---
+++ src/BuildingBlocks/AES.Core/DomainObjects/Cpf.cs
@@ -0,0 +1,16 @@
+﻿namespace AES.Core.DomainObjects
+{
+    public class Cpf
+    {
+        public const int CpfMaxLenght = 11;
+        public string Number { get; private set; }
+
+        // CTOR ofr entity
+        public Cpf() { }
+
+        public Cpf(string number)
+        {
+            Number = number;
+        }
+    }
+}
