b2d0d8cc6b39e235ae01ccb642f712990e0e0b69
Thiago Cruz <thagocruz@gmail.com>
2020-05-23 23:38:35 -0300

    Add entity reference and create constructor

--- src/Services/AES.Client.API/Models/Client.cs
+++ src/Services/AES.Client.API/Models/Client.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AES.Core.DomainObjects;
 
 namespace AES.Client.API.Models
 {
-    public class Client
+    public class Client : Entity
     {
         public string Name { get; private set; }
         public string Email { get; private set; }
         public string Cpf { get; private set; }
         public bool Excluded { get; private set; }
         public Address Address { get; private set; }
+
+        public Client(string name, string email, string cpf)
+        {
+            Name = name;
+            Email = email;
+            Cpf = cpf;
+            Excluded = false;
+        }
     }
 
     public class Address
