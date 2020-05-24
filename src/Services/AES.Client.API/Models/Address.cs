3e7fdaf0521882118cac9300ae86565593d50905
Thiago Cruz <thagocruz@gmail.com>
2020-05-23 23:46:28 -0300

    Move address

---
+++ src/Services/AES.Client.API/Models/Address.cs
@@ -0,0 +1,31 @@
+﻿using AES.Core.DomainObjects;
+using System;
+
+namespace AES.Client.API.Models
+{
+    public class Address : Entity
+    {
+        public string PublicPlace { get; private set; }
+        public string Number { get; private set; }
+        public string Complement { get; private set; }
+        public string Neighborhood { get; private set; }
+        public string Cep { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public Guid ClientId { get; private set; }
+
+        // EF Relation
+        public Client Client { get; private set; }
+
+        public Address(string publicPlace, string number, string complement, string neighborhood, string cep, string city, string state)
+        {
+            PublicPlace = publicPlace;
+            Number = number;
+            Complement = complement;
+            Neighborhood = neighborhood;
+            Cep = cep;
+            City = city;
+            State = state;
+        }
+    }
+}
--- src/Services/AES.Client.API/Models/Client.cs
+++ src/Services/AES.Client.API/Models/Client.cs
@@ -1,5 +1,4 @@
 ﻿using AES.Core.DomainObjects;
-using System;
 
 namespace AES.Client.API.Models
 {
@@ -20,32 +19,6 @@ namespace AES.Client.API.Models
             Email = email;
             Cpf = cpf;
             Excluded = false;
-        }
-    }
-
-    public class Address : Entity
-    {
-        public string PublicPlace { get; private set; }
-        public string Number { get; private set; }
-        public string Complement { get; private set; }
-        public string Neighborhood { get; private set; }
-        public string Cep { get; private set; }
-        public string City { get; private set; }
-        public string State { get; private set; }
-        public Guid ClientId { get; private set; }
-
-        // EF Relation
-        public Client Client { get; private set; }
-
-        public Address(string publicPlace, string number, string complement, string neighborhood, string cep, string city, string state)
-        {
-            PublicPlace = publicPlace;
-            Number = number;
-            Complement = complement;
-            Neighborhood = neighborhood;
-            Cep = cep;
-            City = city;
-            State = state;
         }
     }
 }
