a5458b3ed2940134422985decf07666425ee6285
Thiago Cruz <thagocruz@gmail.com>
2020-05-24 17:30:31 -0300

    Add client script migration

---
+++ sql/InitialClientScript.sql
@@ -0,0 +1,47 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Clients] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] varchar(200) NOT NULL,
+    [Email] varchar(254) NULL,
+    [Cpf] varchar(11) NULL,
+    [Excluded] bit NOT NULL,
+    CONSTRAINT [PK_Clients] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE TABLE [Addresses] (
+    [Id] uniqueidentifier NOT NULL,
+    [PublicPlace] varchar(200) NOT NULL,
+    [Number] varchar(50) NOT NULL,
+    [Complement] varchar(250) NULL,
+    [Neighborhood] varchar(100) NOT NULL,
+    [Cep] varchar(20) NOT NULL,
+    [City] varchar(100) NOT NULL,
+    [State] varchar(50) NOT NULL,
+    [ClientId] uniqueidentifier NOT NULL,
+    CONSTRAINT [PK_Addresses] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Addresses_Clients_ClientId] FOREIGN KEY ([ClientId]) REFERENCES [Clients] ([Id]) ON DELETE NO ACTION
+);
+
+GO
+
+CREATE UNIQUE INDEX [IX_Addresses_ClientId] ON [Addresses] ([ClientId]);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20200524202854_Clients', N'3.1.3');
+
+GO
+
