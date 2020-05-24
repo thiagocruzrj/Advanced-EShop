bec223ec12c09ebe5464ef1501dc393c962bbe7b
Thiago Cruz <thagocruz@gmail.com>
2020-05-24 16:04:09 -0300

    Add string extension to verify numbers

---
+++ src/BuildingBlocks/AES.Core/Tools/StringTools.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace AES.Core.Tools
+{
+    public static class StringTools
+    {
+        public static string OnlyNumbers(this string str, string input)
+        {
+            return new string(input.Where(char.IsDigit).ToArray());
+        }
+    }
+}
