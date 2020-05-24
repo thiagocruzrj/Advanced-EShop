0cf790cd7873f9c569a0cb2519410b9a12e42dc4
Thiago Cruz <thagocruz@gmail.com>
2020-05-24 16:52:47 -0300

    Add DI config extension

---
+++ src/Services/AES.Client.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,13 @@
+﻿using AES.Clients.API.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AES.Clients.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<ClientesContext>();
+        }
+    }
+}
