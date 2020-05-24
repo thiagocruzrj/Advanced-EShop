c9cd72d4b96c9a05f742549af9bbd0fbd5284195
Thiago Cruz <thagocruz@gmail.com>
2020-05-24 16:38:37 -0300

    Address mapping added

---
+++ src/Services/AES.Client.API/Data/Mappings/AddressMapping.cs
@@ -0,0 +1,43 @@
+﻿using AES.Clients.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AES.Clients.API.Data.Mappings
+{
+    public class AddressMapping : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.PublicPlace)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(c => c.Number)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            builder.Property(c => c.Cep)
+                .IsRequired()
+                .HasColumnType("varchar(20)");
+
+            builder.Property(c => c.Complement)
+                .HasColumnType("varchar(250)");
+
+            builder.Property(c => c.Neighborhood)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(c => c.City)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(c => c.State)
+                .IsRequired()
+                .HasColumnType("varchar(50)");
+
+            builder.ToTable("Addresses");
+        }
+    }
+}
