bf3cbf669a1f8cb0b296c0a54be4492006b847dc
Thiago Cruz <thagocruz@gmail.com>
2020-05-25 21:58:31 -0300

    Include base validation property

--- src/BuildingBlocks/AES.Core/Messages/Command.cs
+++ src/BuildingBlocks/AES.Core/Messages/Command.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using FluentValidation.Results;
+using System;
 
 namespace AES.Core.Messages
 {
     public abstract class Command : Message
     {
         public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
         protected Command()
         {
             Timestamp = DateTime.Now;
+        }
+
+        public virtual bool IsValid()
+        {
+            throw new NotImplementedException();
         }
     }
 }
