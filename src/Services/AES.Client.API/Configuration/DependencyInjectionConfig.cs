ca02172640be1534cb117f1161dcd887f9c2b93d
Thiago Cruz <thagocruz@gmail.com>
2020-05-25 22:28:22 -0300

    Add handlers services on DI

--- src/Services/AES.Client.API/Configuration/DependencyInjectionConfig.cs
+++ src/Services/AES.Client.API/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,8 @@
-﻿using AES.Clients.API.Data;
+﻿using AES.Clients.API.Commands;
+using AES.Clients.API.Data;
+using AES.Core.Mediator;
+using FluentValidation.Results;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AES.Clients.API.Configuration
@@ -7,6 +11,9 @@ namespace AES.Clients.API.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+            services.AddScoped<IRequestHandler<RegisterClientCommand, ValidationResult>, ClientCommandHandler>();
+
             services.AddScoped<ClientesContext>();
         }
     }
