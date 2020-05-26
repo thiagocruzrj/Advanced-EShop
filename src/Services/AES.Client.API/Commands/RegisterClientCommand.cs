fe6ab5e835335c55a93f77e735634b541c9ec6a3
Thiago Cruz <thagocruz@gmail.com>
2020-05-25 21:49:18 -0300

    Add register client properties

---
+++ src/Services/AES.Client.API/Commands/RegisterClientCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AES.Clients.API.Commands
+{
+    public class RegisterClientCommand
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+    }
+}
