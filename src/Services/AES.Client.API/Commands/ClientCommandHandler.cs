816093c824c3195304178a1291844d7b52fef2c1
Thiago Cruz <thagocruz@gmail.com>
2020-05-25 22:05:16 -0300

    Add handler client class

---
+++ src/Services/AES.Client.API/Commands/ClientCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace AES.Clients.API.Commands
+{
+    public class ClientCommandHandler
+    {
+        public void Handler(RegisterClientCommand message)
+        {
+            // Validation command
+            // persist in the base for example
+        }
+    }
+}
