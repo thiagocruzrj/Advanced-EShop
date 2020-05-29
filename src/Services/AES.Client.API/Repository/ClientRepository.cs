01d57434e941bea825de76321fc33fbe32de2844
Thiago Cruz <thagocruz@gmail.com>
2020-05-29 00:53:58 -0300

    Add interface methods

--- src/Services/AES.Client.API/Repository/ClientRepository.cs
+++ src/Services/AES.Client.API/Repository/ClientRepository.cs
@@ -1,8 +1,29 @@
 ﻿using AES.Clients.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AES.Clients.API.Repository
 {
     public class ClientRepository : IClientRepository
     {
+        public void Add(Client client)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<Client>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Client> GetByCpf(string cpf)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
