2f31e0ce26228ddc9f03940cd13181eed065e865
Thiago Cruz <thagocruz@gmail.com>
2020-05-31 15:57:53 -0300

    Add handler interface

---
+++ src/Services/AES.Client.API/Application/Events/ClientEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AES.Clients.API.Application.Events
+{
+    public class ClientEventHandler : INotificationHandler<ClientRegisteredEvent>
+    {
+        public Task Handle(ClientRegisteredEvent notification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
