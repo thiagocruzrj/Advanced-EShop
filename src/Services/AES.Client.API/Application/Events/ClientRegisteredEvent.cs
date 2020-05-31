82d81e5bb4aa9701464ca1a54793b10bc7e8b5ab
Thiago Cruz <thagocruz@gmail.com>
2020-05-31 15:56:05 -0300

    Add all client properties on event

---
+++ src/Services/AES.Client.API/Application/Events/ClientRegisteredEvent.cs
@@ -0,0 +1,22 @@
+﻿using AES.Core.Mediator;
+using System;
+
+namespace AES.Clients.API.Application.Events
+{
+    public class ClientRegisteredEvent : Event
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+
+        public ClientRegisteredEvent(Guid id, string name, string email, string cpf)
+        {
+            AggregateId = id;
+            Id = id;
+            Name = name;
+            Email = email;
+            Cpf = cpf;
+        }
+    }
+}
