97d7b88198bd0b150f290205644edf2482b0b96b
Thiago Cruz <thagocruz@gmail.com>
2020-06-05 00:20:23 -0300

    Add cof adapter

--- src/Web/AES.WebApp.MVC/Extensions/CpfAnnotation.cs
+++ src/Web/AES.WebApp.MVC/Extensions/CpfAnnotation.cs
@@ -1,4 +1,7 @@
 ﻿using AES.Core.DomainObjects;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.Extensions.Localization;
 using System.ComponentModel.DataAnnotations;
 
 namespace AES.WebApp.MVC.Extensions
@@ -8,6 +11,21 @@ namespace AES.WebApp.MVC.Extensions
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             return Cpf.Validate(value.ToString()) ? ValidationResult.Success : new ValidationResult("Invalid Cpf format");
+        }
+    }
+
+    public class CpfAttributeAdapter : AttributeAdapterBase<CpfAttribute>
+    {
+        public CpfAttributeAdapter(CpfAttribute attribute, IStringLocalizer stringLocalizer) : base(attribute, stringLocalizer) { }
+
+        public override void AddValidation(ClientModelValidationContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string GetErrorMessage(ModelValidationContextBase validationContext)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
