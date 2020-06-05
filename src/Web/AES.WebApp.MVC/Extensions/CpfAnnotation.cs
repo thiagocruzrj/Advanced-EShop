5496150e2c14d54a74c06ea7f01ee005d243d6f5
Thiago Cruz <thagocruz@gmail.com>
2020-06-05 00:18:26 -0300

    Add cpf validation by attribute

---
+++ src/Web/AES.WebApp.MVC/Extensions/CpfAnnotation.cs
@@ -0,0 +1,13 @@
+﻿using AES.Core.DomainObjects;
+using System.ComponentModel.DataAnnotations;
+
+namespace AES.WebApp.MVC.Extensions
+{
+    public class CpfAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            return Cpf.Validate(value.ToString()) ? ValidationResult.Success : new ValidationResult("Invalid Cpf format");
+        }
+    }
+}
