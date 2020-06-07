0559287634178b5ed3b0b66e5ddd7d5e53ac363e
Thiago Cruz <thagocruz@gmail.com>
2020-06-07 15:38:29 -0300

    Base request pathern

--- src/BuildingBlocks/AES.Core/Messages/Integration/ResponseMessage.cs
+++ src/BuildingBlocks/AES.Core/Messages/Integration/ResponseMessage.cs
@@ -5,5 +5,10 @@ namespace AES.Core.Messages.Integration
     public class ResponseMessage : Message
     {
         public ValidationResult ValidationResult { get; set; }
+
+        public ResponseMessage(ValidationResult validationResult)
+        {
+            ValidationResult = validationResult;
+        }
     }
 }
