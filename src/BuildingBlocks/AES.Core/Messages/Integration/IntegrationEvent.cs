6aa418b98efb382d986aa57b36193de2965992f5
Thiago Cruz <thagocruz@gmail.com>
2020-06-07 15:33:40 -0300

    Add user registered necessary properties

--- src/BuildingBlocks/AES.Core/Messages/Integration/IntegrationEvent.cs
+++ src/BuildingBlocks/AES.Core/Messages/Integration/IntegrationEvent.cs
@@ -1,4 +1,17 @@
-﻿namespace AES.Core.Messages.Integration
+﻿using System;
+
+namespace AES.Core.Messages.Integration
 {
-    public abstract class IntegrationEvent : Event { }
+    public abstract class IntegrationEvent : Event 
+    {
+        
+    }
+
+    public class UserRegisteredIntegrationEvent : IntegrationEvent
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+    }
 }
