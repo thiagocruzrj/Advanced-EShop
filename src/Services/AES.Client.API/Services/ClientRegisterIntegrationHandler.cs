1cfa90cb615d4c42b5b208f534a44814b28abb4d
Thiago Cruz <thagocruz@gmail.com>
2020-06-13 16:18:18 -0300

    Add new background service

---
+++ src/Services/AES.Client.API/Services/ClientRegisterIntegrationHandler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AES.Clients.API.Services
+{
+    public class ClientRegisterIntegrationHandler : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
