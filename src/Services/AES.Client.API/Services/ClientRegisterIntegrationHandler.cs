19d5eb5f2ff8ea489ba1f5d55dcd63708bad0286
Thiago Cruz <thagocruz@gmail.com>
2020-06-13 16:26:44 -0300

    Execulte class base

--- src/Services/AES.Client.API/Services/ClientRegisterIntegrationHandler.cs
+++ src/Services/AES.Client.API/Services/ClientRegisterIntegrationHandler.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using AES.Core.Messages.Integration;
+using EasyNetQ;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,9 +9,21 @@ namespace AES.Clients.API.Services
 {
     public class ClientRegisterIntegrationHandler : BackgroundService
     {
+        private IBus _bus;
+
+        public ClientRegisterIntegrationHandler(IBus bus)
+        {
+            _bus = bus;
+        }
+
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            throw new NotImplementedException();
+            _bus = RabbitHutch.CreateBus("hostlocalhost:5672");
+
+            _bus.RespondAsync<UserRegisteredIntegrationEvent, ResponseMessage>(async request =>
+                new ResponseMessage());
+
+            return Task.CompletedTask;
         }
     }
 }
