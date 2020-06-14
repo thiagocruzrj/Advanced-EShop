91063a403e060372786de2c3d17b68e70c306f3c
Thiago Cruz <thagocruz@gmail.com>
2020-06-14 15:59:51 -0300

    Extract interface

---
+++ src/BuildingBlocks/AES.MessageBus/IMessageBus.cs
@@ -0,0 +1,32 @@
+﻿using AES.Core.Messages.Integration;
+using System;
+using System.Threading.Tasks;
+
+namespace AES.MessageBus
+{
+    public interface IMessageBus : IDisposable
+    {
+        void Publish<T>(T message) where T : IntegrationEvent;
+        Task PublishAsync<T>(T message) where T : IntegrationEvent;
+        void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class;
+        void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class;
+
+        TResponse Request<TRequest, TResponse>(TRequest request)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage;
+
+        Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage;
+
+        IDisposable Respond<TRequest, TResponse>(Func<TRequest, Task<TResponse>> respond)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage;
+
+        IDisposable RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> respond)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage;
+
+        bool IsConnected { get; }
+    }
+}
