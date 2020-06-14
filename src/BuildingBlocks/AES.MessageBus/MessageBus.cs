5212d4a94e8ad253fb0620bb6776013589d8d0ba
Thiago Cruz <thagocruz@gmail.com>
2020-06-13 22:25:30 -0300

    Implemente base IMessage Methods

--- src/BuildingBlocks/AES.MessageBus/MessageBus.cs
+++ src/BuildingBlocks/AES.MessageBus/MessageBus.cs
@@ -9,6 +9,56 @@ namespace AES.MessageBus
     {
         private IBus _bus;
 
+        public bool IsConnected => throw new NotImplementedException();
+
+        public void Publish<T>(T message) where T : IntegrationEvent
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PublishAsync<T>(T message) where T : IntegrationEvent
+        {
+            throw new NotImplementedException();
+        }
+
+        public TResponse Request<TRequest, TResponse>(TRequest request)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable Respond<TRequest, TResponse>(Func<TRequest, Task<TResponse>> respond)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDisposable> RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> respond)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
         public void Dispose()
         {
             throw new NotImplementedException();
