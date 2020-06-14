3e50f9c93bb316fdbc058557a47670fd4aa64ba5
Thiago Cruz <thagocruz@gmail.com>
2020-06-14 16:41:09 -0300

    Add new messagebus config on identity service

---
+++ src/Services/AES.Identity.API/Configuration/MessageBusConfig.cs
@@ -0,0 +1,15 @@
+﻿using AES.Core.Tools;
+using AES.MessageBus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AES.Identity.API.Configuration
+{
+    public static class MessageBusConfig
+    {
+        public static void AddMessageBusConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"));
+        }
+    }
+}
