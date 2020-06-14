5f46072bbcea19b4c60c30589f30c53497ee42fb
Thiago Cruz <thagocruz@gmail.com>
2020-06-13 22:10:17 -0300

    Starting with easy net q

--- src/BuildingBlocks/AES.MessageBus/MessageBus.cs
+++ src/BuildingBlocks/AES.MessageBus/MessageBus.cs
@@ -1,11 +1,19 @@
-﻿namespace AES.MessageBus
+﻿using EasyNetQ;
+using System;
+
+namespace AES.MessageBus
 {
     public class MessageBus : IMessageBus
     {
+        private IBus _bus;
 
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    public interface IMessageBus
+    public interface IMessageBus : IDisposable
     {
 
     }
