c62cc7b2ba691dfd303ca81c1b3b86758157c2e7
Thiago Cruz <thagocruz@gmail.com>
2020-06-14 16:14:19 -0300

    Add DI extension

---
+++ src/BuildingBlocks/AES.MessageBus/DependencyInjectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using EasyNetQ.LightInject;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AES.MessageBus
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceContainer AddMessageBus(this IServiceCollection services, string connection)
+        {
+        }
+    }
+}
