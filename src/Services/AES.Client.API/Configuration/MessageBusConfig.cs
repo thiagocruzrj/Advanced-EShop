ae8ba27b1ea573e7221d794ab3d712fc904970f3
Thiago Cruz <thagocruz@gmail.com>
2020-06-14 16:27:22 -0300

    Add messagequeue connection string extension

--- src/Services/AES.Client.API/Configuration/MessageBusConfig.cs
+++ src/Services/AES.Client.API/Configuration/MessageBusConfig.cs
@@ -7,7 +7,7 @@ namespace AES.Clients.API.Configuration
     {
         public static void AddMessageBusConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
-
+            services.AddMessageBus(configuration.GetConnectionString());
         }
     }
 }
