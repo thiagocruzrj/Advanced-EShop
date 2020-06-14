49e7ec7dd40e7299becb3e388f24f548bb91fc12
Thiago Cruz <thagocruz@gmail.com>
2020-06-13 22:12:56 -0300

    Add subscribe method

--- src/BuildingBlocks/AES.MessageBus/MessageBus.cs
+++ src/BuildingBlocks/AES.MessageBus/MessageBus.cs
@@ -1,6 +1,7 @@
 ﻿using AES.Core.Messages.Integration;
 using EasyNetQ;
 using System;
+using System.Threading.Tasks;
 
 namespace AES.MessageBus
 {
@@ -17,5 +18,8 @@ namespace AES.MessageBus
     public interface IMessageBus : IDisposable
     {
         void Publish<T>(T message) where T : IntegrationEvent;
+        Task PublishAsync<T>(T message) where T : IntegrationEvent;
+        void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class;
+        void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class;
     }
 }
