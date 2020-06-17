29c57c9ebdc12af57b491411cdaa8d743f171431
Thiago Cruz <thagocruz@gmail.com>
2020-06-17 01:26:30 -0300

    Add context on startup

--- src/Services/AES.ShopCart.API/Configuration/ApiConfig.cs
+++ src/Services/AES.ShopCart.API/Configuration/ApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using AES.WebApi.Core.Identity;
+﻿using AES.ShopCart.API.Data;
+using AES.WebApi.Core.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +13,9 @@ namespace AES.ShopCart.API.Configuration
     {
         public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<ShopCartContext>(o =>
+                o.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllers();
 
             services.AddCors(options =>
