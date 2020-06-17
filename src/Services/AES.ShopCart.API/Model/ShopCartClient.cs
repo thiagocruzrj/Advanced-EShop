eb29754dbe04bdc4686870177f2c51d8ea8dff67
Thiago Cruz <thagocruz@gmail.com>
2020-06-17 00:58:46 -0300

    Add necessary properties on ShopCartItem Entity

---
+++ src/Services/AES.ShopCart.API/Model/ShopCartClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AES.ShopCart.API.Model
+{
+    public class ShopCartClient
+    {
+    }
+}
---
+++ src/Services/AES.ShopCart.API/Model/ShopCartItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AES.ShopCart.API.Model
+{
+    public class ShopCartItem
+    {
+        public ShopCartItem(Guid id)
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public Guid ShopCartId { get; set; }
+    }
+}
