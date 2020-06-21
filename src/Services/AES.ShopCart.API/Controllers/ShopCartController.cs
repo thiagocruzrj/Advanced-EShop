5d41bd653b247d3413da19b8e3d01f2018d44fe9
Thiago Cruz <thagocruz@gmail.com>
2020-06-20 23:28:46 -0300

    Add base cart item endpoints

--- src/Services/AES.ShopCart.API/Controllers/ShopCartController.cs
+++ src/Services/AES.ShopCart.API/Controllers/ShopCartController.cs
@@ -1,10 +1,37 @@
 ﻿using AES.Core.Controllers;
+using AES.ShopCart.API.Model;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
 
 namespace AES.ShopCart.API.Controllers
 {
     [Authorize]
     public class ShopCartController : MainController
     {
+        [HttpGet("shopCart")]
+        public async Task<ShopCartClient> GetShopCart()
+        {
+            return null;
+        }
+
+        [HttpPost("shopCart")]
+        public async Task<IActionResult> AddCartItem(ShopCartItem item)
+        {
+            return CustomResponse();
+        }
+
+        [HttpPut("shopCart")]
+        public async Task<IActionResult> UpdateCartItem(Guid productId, ShopCartItem item)
+        {
+            return CustomResponse();
+        }
+
+        [HttpDelete("shopCart")]
+        public async Task<IActionResult> RemoveCartItem(Guid productId)
+        {
+            return CustomResponse();
+        }
     }
 }
