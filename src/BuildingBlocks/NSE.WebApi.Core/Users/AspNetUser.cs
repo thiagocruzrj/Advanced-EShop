49a1a7468699bd84ff8ffd8c417f7a99972ba083
Thiago Cruz <thagocruz@gmail.com>
2020-06-20 23:45:20 -0300

    Create AspNetUser.cs

---
+++ src/BuildingBlocks/NSE.WebApi.Core/Users/AspNetUser.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace AES.WebApi.Core.Users
+{
+    public class AspNetUser : IAspNetUser
+    {
+        private readonly IHttpContextAccessor _accessor;
+
+        public AspNetUser(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        public string Name => _accessor.HttpContext.User.Identity.Name;
+
+        public Guid GetUserId()
+        {
+            return IsAuthenticated() ? Guid.Parse(_accessor.HttpContext.User.GetUserId()) : Guid.Empty;
+        }
+
+        public string GetUserEmail()
+        {
+            return IsAuthenticated() ? _accessor.HttpContext.User.GetUserEmail() : "";
+        }
+
+        public string GetUserToken()
+        {
+            return IsAuthenticated() ? _accessor.HttpContext.User.GetUserToken() : "";
+        }
+
+        public bool IsAuthenticated()
+        {
+            return _accessor.HttpContext.User.Identity.IsAuthenticated;
+        }
+
+        public bool HasRole(string role)
+        {
+            return _accessor.HttpContext.User.IsInRole(role);
+        }
+
+        public IEnumerable<Claim> GetClaims()
+        {
+            return _accessor.HttpContext.User.Claims;
+        }
+
+        public HttpContext GetHttpContext()
+        {
+            return _accessor.HttpContext;
+        }
+    }
+}
