6270567b0ea82395bf5e38228cd70cd3921c4cbc
Thiago Cruz <thagocruz@gmail.com>
2020-07-28 23:18:42 -0300

    Create shop cart view model service interface

---
+++ src/Web/AES.WebApp.MVC/Service/IShopCartService.cs
@@ -0,0 +1,14 @@
+﻿using AES.WebApp.MVC.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace AES.WebApp.MVC.Service
+{
+    public interface IShopCartService
+    {
+        Task<ShopCartViewModel> GetShopCart();
+        Task<ResponseResult> AddItemOnShopCart(ProductItemViewModel product);
+        Task<ResponseResult> UpdateItemOnShopCart(Guid productId, ProductItemViewModel product);
+        Task<ResponseResult> RemoveItemOnShopCart(Guid productId);
+    }
+}
