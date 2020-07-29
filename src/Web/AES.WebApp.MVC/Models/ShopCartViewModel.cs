e4f386d00528f2a0dc19da276350bfc30bfbab23
Thiago Cruz <thagocruz@gmail.com>
2020-07-28 23:07:10 -0300

    Add shop cart view model

---
+++ src/Web/AES.WebApp.MVC/Models/ShopCartViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace AES.WebApp.MVC.Models
+{
+    public class ShopCartViewModel
+    {
+        public decimal TotalPrice { get; set; }
+        public List<ProductItemViewModel> Itens { get; set; } = new List<ProductItemViewModel>();
+    }
+}
