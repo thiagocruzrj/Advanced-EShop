29e36b81e1334de46e261d13944c1fde45a06590
Thiago Cruz <thagocruz@gmail.com>
2020-07-28 23:08:31 -0300

    Add ProductItemViewModel

--- src/Web/AES.WebApp.MVC/Models/ShopCartViewModel.cs
+++ src/Web/AES.WebApp.MVC/Models/ShopCartViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace AES.WebApp.MVC.Models
 {
@@ -6,5 +7,14 @@ namespace AES.WebApp.MVC.Models
     {
         public decimal TotalPrice { get; set; }
         public List<ProductItemViewModel> Itens { get; set; } = new List<ProductItemViewModel>();
+    }
+
+    public class ProductItemViewModel
+    {
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
     }
 }
