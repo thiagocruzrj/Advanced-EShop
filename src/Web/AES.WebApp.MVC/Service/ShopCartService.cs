210d58d69f1b79cdc1897c2e9c6ac8688039b0e5
Thiago Cruz <thagocruz@gmail.com>
2020-07-28 23:25:41 -0300

    Create ShopCartService.cs

---
+++ src/Web/AES.WebApp.MVC/Service/ShopCartService.cs
@@ -0,0 +1,37 @@
+﻿using AES.WebApp.MVC.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AES.WebApp.MVC.Service
+{
+    public class ShopCartService : IShopCartService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ShopCartService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<ShopCartViewModel> GetShopCart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseResult> AddItemOnShopCart(ProductItemViewModel product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseResult> UpdateItemOnShopCart(Guid productId, ProductItemViewModel product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseResult> RemoveItemOnShopCart(Guid productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
