9a0aeab2a2b3876d66c2eef71c7ec4bb794c2bd8
Thiago Cruz <thagocruz@gmail.com>
2020-07-31 15:33:36 -0300

    Create ShopCartViewComponent.cs

---
+++ src/Web/AES.WebApp.MVC/Extensions/ShopCartViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using AES.WebApp.MVC.Models;
+using AES.WebApp.MVC.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AES.WebApp.MVC.Extensions
+{
+    public class ShopCartViewComponent : ViewComponent
+    {
+        private readonly IShopCartService _shopCartService;
+
+        public ShopCartViewComponent(IShopCartService shopCartService)
+        {
+            _shopCartService = shopCartService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _shopCartService.GetShopCart() ?? new ShopCartViewModel());
+        }
+    }
+}
