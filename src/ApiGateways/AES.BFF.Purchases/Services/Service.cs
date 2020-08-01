41ee77b528adb492fec1e3454e7683669a08ec22
Thiago Cruz <thagocruz@gmail.com>
2020-08-01 14:16:48 -0300

    Update Service.cs

--- src/ApiGateways/AES.BFF.Purchases/Services/Service.cs
+++ src/ApiGateways/AES.BFF.Purchases/Services/Service.cs
@@ -1,11 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using AES.WebApp.MVC.Extensions;
+using AES.WebApp.MVC.Models;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace AES.BFF.Purchases.Services
 {
-    public class Service
+    public abstract class Service
     {
+        protected StringContent GetContent(object data)
+        {
+            return new StringContent(
+                JsonSerializer.Serialize(data),
+                Encoding.UTF8,
+                "application/json");
+        }
+
+        protected async Task<T> DeserializeObjectResponse<T>(HttpResponseMessage requestMessage)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            return JsonSerializer.Deserialize<T>(await requestMessage.Content.ReadAsStringAsync(), options);
+        }
+
+        protected bool HandlingErrorsReponse(HttpResponseMessage response)
+        {
+            if (response.StatusCode == HttpStatusCode.BadRequest) return false;
+
+            response.EnsureSuccessStatusCode();
+            return true;
+        }
     }
 }
