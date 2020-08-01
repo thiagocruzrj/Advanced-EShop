04b8642209067b2d55f9f9239dfd4c9e7e86e94e
Thiago Cruz <thagocruz@gmail.com>
2020-08-01 13:11:42 -0300

    Add configuration to access all apis

---
+++ src/ApiGateways/AES.BFF.Purchases/Extensions/AppServicesSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace AES.BFF.Purchases.Extensions
+{
+    public class AppServicesSettings
+    {
+        public string CatalogUrl { get; set; }
+        public string ShopCartUrl { get; set; }
+        public string OrderUrl { get; set; }
+        public string PaymentUrl { get; set; }
+    }
+}
