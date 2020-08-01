fa7b48818d6854986fd8058867e93b326f86068d
Thiago Cruz <thagocruz@gmail.com>
2020-08-01 13:10:20 -0300

    Add cors

---
+++ src/ApiGateways/AES.BFF.Purchases/Configuration/ApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AES.BFF.Purchases.Configuration
+{
+    public static class ApiConfig
+    {
+        public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+            services.Configure<AppServiceSettings>(configuration);
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Total", builder =>
+                                  builder.AllowAnyOrigin()
+                                         .AllowAnyMethod()
+                                         .AllowAnyHeader());
+            });
+        }
+    }
+}
