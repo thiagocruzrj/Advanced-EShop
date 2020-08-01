f68b48cea299c559d9d02f2dcc38d55ca9a43110
Thiago Cruz <thagocruz@gmail.com>
2020-08-01 13:17:43 -0300

    Create MessageBusConfig.cs

---
+++ src/ApiGateways/AES.BFF.Purchases/Configuration/MessageBusConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AES.BFF.Purchases.Configuration
+{
+    public static class MessageBusConfig
+    {
+        public static void AddMessageBusConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
