a75e826c6527430b356a59c849839bc619cb35f3
Thiago Cruz <thagocruz@gmail.com>
2020-08-01 13:25:37 -0300

    Create ShopCartController.cs

---
+++ src/ApiGateways/AES.BFF.Purchases/Controllers/ShopCartController.cs
@@ -0,0 +1,18 @@
+﻿using AES.Core.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AES.BFF.Purchases.Controllers
+{
+    [Authorize]
+    public class ShopCartController : MainController
+    {
+        [HttpGet]
+        [Route("purchases/shopCart")]
+        public async Task<IActionResult> Index()
+        {
+            return CustomResponse();
+        }
+    }
+}
