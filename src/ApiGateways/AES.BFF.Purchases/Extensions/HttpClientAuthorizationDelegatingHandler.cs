51384cf6b451801bbe4d470dbeb95ca345584649
Thiago Cruz <thagocruz@gmail.com>
2020-08-01 13:33:56 -0300

    Create HttpClientAuthorizationDelegatingHandler.cs

---
+++ src/ApiGateways/AES.BFF.Purchases/Extensions/HttpClientAuthorizationDelegatingHandler.cs
@@ -0,0 +1,15 @@
+﻿using AES.WebApi.Core.Users;
+using System.Net.Http;
+
+namespace AES.BFF.Purchases.Extensions
+{
+    public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
+    {
+        private readonly IAspNetUser _aspNetUser;
+
+        public HttpClientAuthorizationDelegatingHandler(IAspNetUser aspNetUser)
+        {
+            _aspNetUser = aspNetUser;
+        }
+    }
+}
