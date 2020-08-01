6475203dd95f92e479db7701903dbd5ea449d330
Thiago Cruz <thagocruz@gmail.com>
2020-08-01 13:16:30 -0300

    Add DI extencion method

---
+++ src/ApiGateways/AES.BFF.Purchases/Configuration/DependendyInjectionConfig.cs
@@ -0,0 +1,15 @@
+﻿using AES.WebApi.Core.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AES.BFF.Purchases.Configuration
+{
+    public static class DependendyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();
+        }
+    }
+}
