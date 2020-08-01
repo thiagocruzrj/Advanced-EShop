dd2c4fe983585b4d4e5ea2583994c2b0529bdee9
Thiago Cruz <thagocruz@gmail.com>
2020-08-01 13:47:58 -0300

    Update HttpClientAuthorizationDelegatingHandler.cs

--- src/ApiGateways/AES.BFF.Purchases/Extensions/HttpClientAuthorizationDelegatingHandler.cs
+++ src/ApiGateways/AES.BFF.Purchases/Extensions/HttpClientAuthorizationDelegatingHandler.cs
@@ -1,5 +1,9 @@
 ﻿using AES.WebApi.Core.Users;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AES.BFF.Purchases.Extensions
 {
@@ -10,6 +14,21 @@ namespace AES.BFF.Purchases.Extensions
         public HttpClientAuthorizationDelegatingHandler(IAspNetUser aspNetUser)
         {
             _aspNetUser = aspNetUser;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var authorizationHeader = _aspNetUser.GetHttpContext().Request.Headers["Authorization"];
+
+            if (!string.IsNullOrEmpty(authorizationHeader))
+                request.Headers.Add("Authorization", new List<string>() { authorizationHeader });
+
+            var token = _aspNetUser.GetUserToken();
+
+            if (token != null)
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            return await base.SendAsync(request, cancellationToken);
         }
     }
 }
