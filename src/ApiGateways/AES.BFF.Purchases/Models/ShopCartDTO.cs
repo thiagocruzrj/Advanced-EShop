507c2523e15b973c125032fe75400270bfc73fda
Thiago Cruz <thagocruz@gmail.com>
2020-08-02 13:25:28 -0300

    Extract dtos

---
+++ src/ApiGateways/AES.BFF.Purchases/Models/ShopCartDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AES.BFF.Purchases.Models
+{
+    public class ShopCartDTO
+    {
+        public decimal TotalPrice { get; set; }
+        public decimal Discount { get; set; }
+        public List<ShopCartItemDTO> Items { get; set; } = new List<ShopCartItemDTO>();
+    }
+}
--- src/ApiGateways/AES.BFF.Purchases/Models/ShopCartItemDTO.cs
+++ src/ApiGateways/AES.BFF.Purchases/Models/ShopCartItemDTO.cs
@@ -2,12 +2,12 @@
 
 namespace AES.BFF.Purchases.Models
 {
-    public class ProductItemDTO
+    public class ShopCartItemDTO
     {
-        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Image { get; set; }
-        public int StockQuantity { get; set; }
+        public int Quantity { get; set; }
     }
 }
