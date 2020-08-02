3fb2d2c007b00f1b83bcdff08170ccba39b7f186
Thiago Cruz <thagocruz@gmail.com>
2020-08-02 13:39:07 -0300

    Create ResponseResult.cs

---
+++ src/BuildingBlocks/AES.Core/Communication/ResponseResult.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace AES.Core.Communication
+{
+    public class ResponseResult
+    {
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public ResponseErrorMessages Errors { get; set; }
+
+        public ResponseResult()
+        {
+            Errors = new ResponseErrorMessages();
+        }
+    }
+
+    public class ResponseErrorMessages
+    {
+        public ResponseErrorMessages()
+        {
+            Messages = new List<string>();
+        }
+        public List<string> Messages { get; set; }
+    }
+}
