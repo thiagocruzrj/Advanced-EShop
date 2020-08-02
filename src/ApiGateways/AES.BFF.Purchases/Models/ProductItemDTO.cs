8486beaa8ae175ad76f2a28ab9e575c0e3eec681
Thiago Cruz <thagocruz@gmail.com>
2020-08-02 13:24:08 -0300

    Add ProductItemDTO

---
+++ src/ApiGateways/AES.BFF.Purchases/Models/ProductItemDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AES.BFF.Purchases.Models
+{
+    public class ProductItemDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public int StockQuantity { get; set; }
+    }
+}
