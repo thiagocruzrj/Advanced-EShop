a820ef9ac0a894f7d4a0af367cfe6b1ba962a5c9
Thiago Cruz <thagocruz@gmail.com>
2020-08-02 13:24:19 -0300

    Add ShopCartItemDTO

--- src/ApiGateways/AES.BFF.Purchases/Models/ProductItemDTO.cs
+++ src/ApiGateways/AES.BFF.Purchases/Models/ProductItemDTO.cs
@@ -11,4 +11,13 @@ namespace AES.BFF.Purchases.Models
         public string Image { get; set; }
         public int StockQuantity { get; set; }
     }
+
+    public class ShopCartItemDTO
+    {
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public int Quantity { get; set; }
+    }
 }
