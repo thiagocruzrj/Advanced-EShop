589aed1a824024c2e06ed6f5f9f87361dccf3587
Thiago Cruz <thagocruz@gmail.com>
2020-08-03 11:46:46 -0300

    Change interface location

---
+++ src/ApiGateways/AES.BFF.Purchases/Services/IShopCartService.cs
@@ -0,0 +1,16 @@
+﻿using AES.BFF.Purchases.Models;
+using AES.Core.Communication;
+using System;
+using System.Threading.Tasks;
+
+namespace AES.BFF.Purchases.Services
+{
+    public interface IShopCartService 
+    {
+        Task<ShopCartDTO> GetShopCart();
+        Task<int> GetShopCartQuantity();
+        Task<ResponseResult> AddItemOnShopCart(ShopCartItemDTO product);
+        Task<ResponseResult> UpdateItemOnShopCart(Guid productId, ShopCartItemDTO product);
+        Task<ResponseResult> RemoveItemOnShopCart(Guid productId);
+    }
+}
--- src/ApiGateways/AES.BFF.Purchases/Services/ShopCartService.cs
+++ src/ApiGateways/AES.BFF.Purchases/Services/ShopCartService.cs
@@ -8,15 +8,6 @@ using System.Threading.Tasks;
 
 namespace AES.BFF.Purchases.Services
 {
-    public interface IShopCartService 
-    {
-        Task<ShopCartDTO> GetShopCart();
-        Task<int> GetShopCartQuantity();
-        Task<ResponseResult> AddItemOnShopCart(ShopCartItemDTO product);
-        Task<ResponseResult> UpdateItemOnShopCart(Guid productId, ShopCartItemDTO product);
-        Task<ResponseResult> RemoveItemOnShopCart(Guid productId);
-    }
-
     public class ShopCartService : Service, IShopCartService
     {
         private readonly HttpClient _httpClient;
