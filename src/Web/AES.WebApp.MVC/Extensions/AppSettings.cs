c9c0ffae6e5d7a0ddce400c5c763f4ced8e4f245
Thiago Cruz <thagocruz@gmail.com>
2020-08-06 16:07:45 -0300

    Update AppSettings.cs

--- src/Web/AES.WebApp.MVC/Extensions/AppSettings.cs
+++ src/Web/AES.WebApp.MVC/Extensions/AppSettings.cs
@@ -4,7 +4,6 @@
     {
         public string AuthUrl { get; set; }
         public string CatalogUrl { get; set; }
-        public string ShopCartUrl { get; set; }
         public string PurchasesBffUrl { get; set; }
     }
 }
