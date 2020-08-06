c68f405c40921da5246a8ca313f3fbb002636210
Thiago Cruz <thagocruz@gmail.com>
2020-08-06 15:05:39 -0300

    Add new injections

--- src/ApiGateways/AES.BFF.Purchases/Configuration/DependendyInjectionConfig.cs
+++ src/ApiGateways/AES.BFF.Purchases/Configuration/DependendyInjectionConfig.cs
@@ -1,6 +1,11 @@
-﻿using AES.WebApi.Core.Users;
+﻿using AES.BFF.Purchases.Extensions;
+using AES.BFF.Purchases.Services;
+using AES.WebApi.Core.Users;
+using AES.WebApp.MVC.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using System;
 
 namespace AES.BFF.Purchases.Configuration
 {
@@ -10,6 +15,18 @@ namespace AES.BFF.Purchases.Configuration
         {
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
+
+            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+
+            services.AddHttpClient<ICatalogService, CatalogService>()
+                    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+                    .AddPolicyHandler(PollyExtentions.WaitTry())
+                    .AddTransientHttpErrorPolicy(p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
+
+            services.AddHttpClient<IShopCartService, ShopCartService>()
+                    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+                    .AddPolicyHandler(PollyExtentions.WaitTry())
+                    .AddTransientHttpErrorPolicy(p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
         }
     }
 }
