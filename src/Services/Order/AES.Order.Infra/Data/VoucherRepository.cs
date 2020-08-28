a6ab664aa97463c10068a5dbb6f6e05d6ad28c6f
Thiago Cruz <thagocruz@gmail.com>
2020-08-28 10:33:41 -0300

    Create VoucherRepository.cs

---
+++ src/Services/Order/AES.Order.Infra/Data/VoucherRepository.cs
@@ -0,0 +1,15 @@
+﻿using AES.Core.Data;
+using AES.Order.Domain.Vouchers;
+
+namespace AES.Order.Infra.Data
+{
+    public class VoucherRepository : IVoucherRepository
+    {
+        public IUnitOfWork UnitOfWork => throw new System.NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
