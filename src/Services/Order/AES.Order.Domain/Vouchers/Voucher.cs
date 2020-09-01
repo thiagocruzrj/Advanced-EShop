e3fd3d92d49c8faef02af589fa4916fb2d395a86
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 20:57:11 -0300

    Add voucher properties

--- src/Services/Order/AES.Order.Domain/Vouchers/Voucher.cs
+++ src/Services/Order/AES.Order.Domain/Vouchers/Voucher.cs
@@ -1,8 +1,19 @@
 ﻿using AES.Core.DomainObjects;
+using System;
 
 namespace AES.Order.Domain.Vouchers
 {
     public class Voucher : Entity, IAggregateRoot
     {
+        public string Code { get; private private set; }
+        public decimal? Percent { get; private set; }
+        public decimal? DiscountValue { get; private set; }
+        public int Quantity { get; private set; }
+        public TypeDiscountVoucher DiscountVoucher { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public DateTime? UseDate { get; private set; }
+        public DateTime ExpirationDate { get; private set; }
+        public bool Active { get; private set; }
+        public bool Used { get; private set; }
     }
 }
