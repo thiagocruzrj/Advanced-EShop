ceb0da6fcc20073ecca12a8d0043d035bd880c6c
Thiago Cruz <thagocruz@gmail.com>
2020-09-02 11:06:50 -0300

    Implemnte GetVoucherByCode

--- src/Services/Order/AES.Order.Infra/Data/VoucherRepository.cs
+++ src/Services/Order/AES.Order.Infra/Data/VoucherRepository.cs
@@ -1,15 +1,29 @@
 ﻿using AES.Core.Data;
 using AES.Order.Domain.Vouchers;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace AES.Order.Infra.Data
 {
     public class VoucherRepository : IVoucherRepository
     {
-        public IUnitOfWork UnitOfWork => throw new System.NotImplementedException();
+        private readonly OrderContext _context;
+
+        public VoucherRepository(OrderContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+        
+        public async Task<Voucher> GetVoucherByCode(string code)
+        {
+            return await _context.Vouchers.FirstOrDefaultAsync(c => c.Code == code);
+        }
 
         public void Dispose()
         {
-            throw new System.NotImplementedException();
+            _context.Dispose();
         }
     }
 }
