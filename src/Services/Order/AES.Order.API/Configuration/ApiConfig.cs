6db29c39685b9b6456f334b915724f35cac67383
Thiago Cruz <thagocruz@gmail.com>
2020-09-02 10:31:52 -0300

    Create ApiConfig.cs

---
+++ src/Services/Order/AES.Order.API/Configuration/ApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using AES.Order.Infra;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AES.Order.API.Configuration
+{
+    public static class ApiConfig
+    {
+        public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(o => o.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+
+            services.AddCors(o =>
+            {
+                o.AddPolicy("Total",
+                    b => b.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyMethod());
+            });
+        }
+    }
+}
