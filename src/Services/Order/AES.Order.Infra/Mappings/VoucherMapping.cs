ddfb918ff1e14cfb1882d48aa97306707d0327b8
Thiago Cruz <thagocruz@gmail.com>
2020-09-01 21:08:32 -0300

    Create VoucherMapping.cs

---
+++ src/Services/Order/AES.Order.Infra/Mappings/VoucherMapping.cs
@@ -0,0 +1,20 @@
+﻿using AES.Order.Domain.Vouchers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AES.Order.Infra.Mappings
+{
+    public class VoucherMapping : IEntityTypeConfiguration<Voucher>
+    {
+        public void Configure(EntityTypeBuilder<Voucher> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Code)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.ToTable("Vouchers");
+        }
+    }
+}
