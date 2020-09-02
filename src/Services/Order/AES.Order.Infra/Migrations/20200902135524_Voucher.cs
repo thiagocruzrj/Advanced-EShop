7f0671c6a70d7c055e9898723bcf70c7c5ebda7c
Thiago Cruz <thagocruz@gmail.com>
2020-09-02 10:58:21 -0300

    Add order migration

---
+++ src/Services/Order/AES.Order.Infra/Migrations/20200902135524_Voucher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AES.Order.Infra.Migrations
+{
+    public partial class Voucher : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vouchers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Code = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Percent = table.Column<decimal>(nullable: true),
+                    DiscountValue = table.Column<decimal>(nullable: true),
+                    Quantity = table.Column<int>(nullable: false),
+                    DiscountVoucher = table.Column<int>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false),
+                    UseDate = table.Column<DateTime>(nullable: true),
+                    ExpirationDate = table.Column<DateTime>(nullable: false),
+                    Active = table.Column<bool>(nullable: false),
+                    Used = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vouchers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vouchers");
+        }
+    }
+}
