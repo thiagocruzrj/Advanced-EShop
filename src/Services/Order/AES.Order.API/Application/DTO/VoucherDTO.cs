0e1badb9fc9060552e9527c047554e33e22c2fe4
Thiago Cruz <thagocruz@gmail.com>
2020-09-02 11:13:30 -0300

    Create VoucherDTO.cs

---
+++ src/Services/Order/AES.Order.API/Application/DTO/VoucherDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace AES.Order.API.Application.DTO
+{
+    public class VoucherDTO
+    {
+        public string Code { get; set; }
+        public decimal? Percent { get; set; }
+        public decimal? DiscountValue { get; set; }
+        public int DiscountValue { get; set; }
+    }
+}
