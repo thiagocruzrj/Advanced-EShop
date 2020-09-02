10b1da1f40b84f8471d4f861653e9464d26d4255
Thiago Cruz <thagocruz@gmail.com>
2020-09-02 11:08:20 -0300

    Create VoucherController.cs

---
+++ src/Services/Order/AES.Order.API/Controllers/VoucherController.cs
@@ -0,0 +1,11 @@
+﻿using AES.Core.Controllers;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AES.Order.API.Controllers
+{
+    [Authorize]
+    public class VoucherController : MainController
+    {
+
+    }
+}
