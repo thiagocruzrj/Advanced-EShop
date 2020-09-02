1fe006d7e23748ede8899f3ed70e63ebb62d7f04
Thiago Cruz <thagocruz@gmail.com>
2020-09-02 11:06:42 -0300

    Add GetVoucherByCode

--- src/Services/Order/AES.Order.Domain/Vouchers/IVoucherRepository.cs
+++ src/Services/Order/AES.Order.Domain/Vouchers/IVoucherRepository.cs
@@ -1,6 +1,10 @@
 ﻿using AES.Core.Data;
+using System.Threading.Tasks;
 
 namespace AES.Order.Domain.Vouchers
 {
-    public interface IVoucherRepository : IRepository<Voucher> { }
+    public interface IVoucherRepository : IRepository<Voucher> 
+    {
+        Task<Voucher> GetVoucherByCode(string code);
+    }
 }
