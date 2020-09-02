ef24f764b86ba204198ee4cb2c15deb864c7c602
Thiago Cruz <thagocruz@gmail.com>
2020-09-02 11:44:38 -0300

    Create VoucherSpec.cs

---
+++ src/Services/Order/AES.Order.Domain/Vouchers/Specs/VoucherSpec.cs
@@ -0,0 +1,14 @@
+﻿using NetDevPack.Specification;
+using System;
+using System.Linq.Expressions;
+
+namespace AES.Order.Domain.Vouchers.Specs
+{
+    public class VoucherDataSpecification : Specification<Voucher>
+    {
+        public override Expression<Func<Voucher, bool>> ToExpression()
+        {
+            return voucher => voucher.ExpirationDate >= DateTime.Now;
+        }
+    }
+}
