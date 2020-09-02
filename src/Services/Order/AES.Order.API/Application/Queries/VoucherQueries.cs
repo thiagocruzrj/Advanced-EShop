6a4edf6febde8b8f0d535c3eddc80b57c7b46d83
Thiago Cruz <thagocruz@gmail.com>
2020-09-02 11:14:59 -0300

    Create VoucherQueries.cs

---
+++ src/Services/Order/AES.Order.API/Application/Queries/VoucherQueries.cs
@@ -0,0 +1,14 @@
+﻿using AES.Order.API.Application.DTO;
+using System.Threading.Tasks;
+
+namespace AES.Order.API.Application.Queries
+{
+    public interface IVoucherQueries
+    {
+        Task<VoucherDTO> GetVoucherByCode(string code);
+    }
+
+    public class VoucherQueries
+    {
+    }
+}
