5f03f3be7a60d2a9f6d93872b76c245d13f4b084
Thiago Cruz <thagocruz@gmail.com>
2020-09-02 10:42:03 -0300

    Add DependencyInjectionConfig class

---
+++ src/Services/Order/AES.Order.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,27 @@
+﻿using AES.Core.Mediator;
+using AES.Order.Domain.Vouchers;
+using AES.Order.Infra;
+using AES.Order.Infra.Data;
+using AES.WebApi.Core.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AES.Order.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            // API
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();
+
+            // Application
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+
+            // Data
+            services.AddScoped<IVoucherRepository, VoucherRepository>();
+            services.AddScoped<OrderContext>();
+        }
+    }
+}
