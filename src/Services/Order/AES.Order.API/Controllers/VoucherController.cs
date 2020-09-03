fd522b3c633e87a9172b958eb98e9fd075e71af6
Thiago Cruz <thagocruz@gmail.com>
2020-09-03 16:21:39 -0300

    Add GetByCode

--- src/Services/Order/AES.Order.API/Controllers/VoucherController.cs
+++ src/Services/Order/AES.Order.API/Controllers/VoucherController.cs
@@ -1,6 +1,8 @@
 ﻿using AES.Core.Controllers;
 using AES.Order.API.Application.Queries;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace AES.Order.API.Controllers
 {
@@ -12,6 +14,15 @@ namespace AES.Order.API.Controllers
         public VoucherController(IVoucherQueries voucherQueries)
         {
             _voucherQueries = voucherQueries;
+        }
+
+        public async Task<IActionResult> GetByCode(string code)
+        {
+            if (string.IsNullOrEmpty(code)) return NotFound();
+
+            var voucher = await _voucherQueries.GetVoucherByCode(code);
+
+            return voucher == null ? NotFound() : CustomResponse();
         }
     }
 }
