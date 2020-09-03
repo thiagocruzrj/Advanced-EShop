62ae3c66f97fbadf1020d19737a11bc80c2a89b9
Thiago Cruz <thagocruz@gmail.com>
2020-09-03 16:16:56 -0300

    Add VoucherQuery Injection on controller

--- src/Services/Order/AES.Order.API/Controllers/VoucherController.cs
+++ src/Services/Order/AES.Order.API/Controllers/VoucherController.cs
@@ -1,4 +1,5 @@
 ﻿using AES.Core.Controllers;
+using AES.Order.API.Application.Queries;
 using Microsoft.AspNetCore.Authorization;
 
 namespace AES.Order.API.Controllers
@@ -6,6 +7,11 @@ namespace AES.Order.API.Controllers
     [Authorize]
     public class VoucherController : MainController
     {
+        private readonly IVoucherQueries _voucherQueries;
 
+        public VoucherController(IVoucherQueries voucherQueries)
+        {
+            _voucherQueries = voucherQueries;
+        }
     }
 }
