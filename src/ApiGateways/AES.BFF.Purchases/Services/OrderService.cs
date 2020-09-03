68efdb98bf2d050b61003761b776aadb25469795
Thiago Cruz <thagocruz@gmail.com>
2020-09-03 16:58:18 -0300

    Add GetVoucherByCode bff implementation

--- src/ApiGateways/AES.BFF.Purchases/Services/OrderService.cs
+++ src/ApiGateways/AES.BFF.Purchases/Services/OrderService.cs
@@ -1,15 +1,19 @@
 ﻿using AES.BFF.Purchases.Extensions;
+using AES.BFF.Purchases.Models;
 using Microsoft.Extensions.Options;
 using System;
+using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace AES.BFF.Purchases.Services
 {
     public interface IOrderService
     {
+        Task<VoucherDTO> GetVoucherByCode(string code);
     }
 
-    public class OrderService : IOrderService
+    public class OrderService : Service, IOrderService
     {
         private readonly HttpClient _httpClient;
 
@@ -17,6 +21,17 @@ namespace AES.BFF.Purchases.Services
         {
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(settings.Value.OrderUrl);
+        }
+
+        public async Task<VoucherDTO> GetVoucherByCode(string code)
+        {
+            var response = await _httpClient.GetAsync($"voucher/{code}");
+
+            if (response.StatusCode == HttpStatusCode.NotFound) return null;
+
+            HandlingErrorsReponse(response);
+
+            return await DeserializeObjectResponse<VoucherDTO>(response);
         }
     }
 }
