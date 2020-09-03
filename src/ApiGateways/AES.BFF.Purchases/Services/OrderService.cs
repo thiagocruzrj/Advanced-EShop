2be658b327c937f7da7fc8a358f05bdfce034dd3
Thiago Cruz <thagocruz@gmail.com>
2020-09-03 16:46:05 -0300

    Create OrderService.cs

---
+++ src/ApiGateways/AES.BFF.Purchases/Services/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using AES.BFF.Purchases.Extensions;
+using Microsoft.Extensions.Options;
+using System;
+using System.Net.Http;
+
+namespace AES.BFF.Purchases.Services
+{
+    public interface IOrderService
+    {
+    }
+
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _httpClient;
+
+        public OrderService(HttpClient httpClient, IOptions<AppServicesSettings> settings)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(settings.Value.OrderUrl);
+        }
+    }
+}
