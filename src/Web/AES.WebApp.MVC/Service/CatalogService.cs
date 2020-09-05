22f8561248ddb61b9117652eae75d922462c458e
Thiago Cruz <thagocruz@gmail.com>
2020-09-05 00:16:42 -0300

    Update CatalogService.cs

--- src/Web/AES.WebApp.MVC/Service/CatalogService.cs
+++ src/Web/AES.WebApp.MVC/Service/CatalogService.cs
@@ -1,6 +1,7 @@
 ﻿using AES.WebApp.MVC.Extensions;
 using AES.WebApp.MVC.Models;
 using Microsoft.Extensions.Options;
+using Refit;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -8,6 +9,21 @@ using System.Threading.Tasks;
 
 namespace AES.WebApp.MVC.Service
 {
+    public interface ICatalogService
+    {
+        Task<IEnumerable<ProductViewModel>> GetAll();
+        Task<ProductViewModel> GetById(Guid id);
+    }
+
+    public interface ICatalogoServiceRefit
+    {
+        [Get("/catalogo/produtos/")]
+        Task<IEnumerable<ProductViewModel>> ObterTodos();
+
+        [Get("/catalogo/produtos/{id}")]
+        Task<ProductViewModel> ObterPorId(Guid id);
+    }
+
     public class CatalogService : Service, ICatalogService
     {
         private readonly HttpClient _httpClient;
