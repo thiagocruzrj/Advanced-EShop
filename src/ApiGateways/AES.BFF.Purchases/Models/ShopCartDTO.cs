e3bc74974657c8279f4ca5187c44548e290b7077
Thiago Cruz <thagocruz@gmail.com>
2020-09-05 00:31:47 -0300

    Update ShopCartDTO.cs

--- src/ApiGateways/AES.BFF.Purchases/Models/ShopCartDTO.cs
+++ src/ApiGateways/AES.BFF.Purchases/Models/ShopCartDTO.cs
@@ -5,7 +5,9 @@ namespace AES.BFF.Purchases.Models
     public class ShopCartDTO
     {
         public decimal TotalPrice { get; set; }
+        public VoucherDTO Voucher { get; set; }
         public decimal Discount { get; set; }
+        public bool UsedVoucher { get; set; }
         public List<ShopCartItemDTO> Items { get; set; } = new List<ShopCartItemDTO>();
     }
 }
