f9b02968f074370211907ae70f21a678973ac454
Thiago Cruz <thagocruz@gmail.com>
2020-09-04 23:02:09 -0300

    Create Voucher.cs

---
+++ src/Services/AES.ShopCart.API/Model/Voucher.cs
@@ -0,0 +1,10 @@
+﻿namespace AES.ShopCart.API.Model
+{
+    public class Voucher
+    {
+        public string Code { get; set; }
+        public decimal? Percent { get; set; }
+        public decimal? DiscountValue { get; set; }
+        public DiscountTypeVoucher DiscountType { get; set; }
+    }
+}
